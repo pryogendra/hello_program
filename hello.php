b299838e9fbf44db3ffda8260f11e3c1b534ff4a
Yogendra Prasad <127005415+pryogendra@users.noreply.github.com>
2023-03-06 08:25:42 +0530

    Add files via upload

---
+++ hello.php
@@ -0,0 +1,11 @@
+<html>
+    <body>
+        <?php
+        $a=5;
+        $b=10;
+        $c=$a+$b;
+        print"Hello student<br>";
+        echo"The addition of ",$a," and",$b," is : ",$c;
+        ?>
+    </body>
+</html>
